51a7afc9cd2119a50be2ba13b2326d13c1b61edb
“Ewin” <ewinoaks@gmail.com>
2024-05-13 14:13:51 +0800

    Route & Navigation

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,11 +1,34 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+
+      {isLoading && (
+        <ActivityIndicator
+          animating={isLoading}
+          color="#fff"
+          size="small"
+          className="ml-2"
+        />
+      )}
+    </TouchableOpacity>
   );
 };
 
--- constants/images.js
+++ constants/images.js
@@ -6,4 +6,12 @@ import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default {
+  profile,
+  thumbnail,
+  cards,
+  path,
+  logo,
+  logoSmall,
+  empty,
+};
